970b744be86b7b76f3e4daeb4bbf2c84248b3b66
Shane Manny <mannyshane81@gmail.com>
2023-03-01 22:42:06 +0100

    background and grass (long)

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ import pygame
 pygame.init()
 
 height = 400
-width = 600
+width = 1200
 
 display_surface = pygame.display.set_mode((width, height))
 
-forest_Background = pygame.image.load("/home/shane/Resources/Pictures/Backgrounds/forestBackground.png").convert()
-grass1 = pygame.image.load("/home/shane/Resources/Pictures/Backgrounds/grass.png").convert_alpha()
-grass2 = pygame.image.load("/home/shane/Resources/Pictures/Backgrounds/grass.png").convert_alpha()
-grass3 = pygame.image.load("/home/shane/Resources/Pictures/Backgrounds/grass.png").convert_alpha()
+forest_Background = pygame.image.load("/home/shane/Resources/Pictures/Backgrounds/forest_long.png").convert()
+grass = pygame.image.load("/home/shane/Resources/Pictures/Backgrounds/grass.png").convert_alpha()
 
 
 pygame.display.set_caption('RandomGame')
 
 while True:
     display_surface.blit(forest_Background, (0, 0))
-    display_surface.blit(grass1,(350,300))
-    display_surface.blit(grass2,(150,300))
-    display_surface.blit(grass3,(-50,300))
+    display_surface.blit(grass,(350,300))
+    display_surface.blit(grass,(150,300))
+    display_surface.blit(grass,(-50,300))
+    display_surface.blit(grass,(550,300))
+    display_surface.blit(grass,(750,300))
+    display_surface.blit(grass,(950,300))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
